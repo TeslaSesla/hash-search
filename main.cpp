18c5bd7074b96cb39dbae3aa8448eaf9aed8fd77
Nikolay Tetus <nikolaytetus@gmail.com>
2021-01-13 04:20:51 +0300

    Refactoring & Added some text

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 
 #include "hash-library/sha256.h"
+#define ITERATIONS_DEFAULT 1000
 
 using namespace std;
 
@@ -30,16 +31,39 @@ int main()
 
     int ch_amount = 2;
     char ch = 'a';
+    unsigned long long max_iterations = 0;
+
+
+    cout << "Enter char, that will be used in string: ";
+    cin >> ch;
+    cout << "Enter amount of chars in string: ";
+    cin >> ch_amount;
+    cout << "Enter a iterations amount [" << ITERATIONS_DEFAULT << "]: ";
+    cin >> max_iterations;
+
+    if (max_iterations == 0)
+        max_iterations = ITERATIONS_DEFAULT;
+
+    string search_string = get_chars_string(ch_amount, ch);
+
+    cout << "Programm will search something like this: " << search_string << "..." << endl
+         << "In a " << max_iterations << " iterations." << endl;
 
     string hash_str = "";
+    unsigned long long matched_lines = 0;
 
-    for (int i = 0; i < INT_MAX - 1; i++)
+    for (unsigned long long i = 0; i < max_iterations; i++)
     {
         hash_str = sha256(to_string(i));
 
-        if (validate_hash(hash_str, "0123456"))
-            cout << i << ") " << hash_str << endl;
+        if (validate_hash(hash_str, search_string))
+        {
+            cout << i << " - " << hash_str << endl;
+            matched_lines++;
+        }
     }
+
+    cout << endl << "Found matching lines: " << matched_lines << endl;
 
     return 0;
 }
